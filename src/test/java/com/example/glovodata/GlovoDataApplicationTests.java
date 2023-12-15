b23989b0fb6ef49b8f9cbc1480a7f2aea95afa0e
Anna Padun <roananik1988@gmail.com>
2023-12-15 12:09:06 +0100

    first init

---
+++ src/test/java/com/example/glovodata/GlovoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glovodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
