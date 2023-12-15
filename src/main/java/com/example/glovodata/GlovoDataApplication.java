349b4bd501170e6af41ab5f6aa3935b35e0ba2da
Anna Padun <roananik1988@gmail.com>
2023-12-15 12:08:58 +0100

    first init

---
+++ src/main/java/com/example/glovodata/GlovoDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.glovodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoDataApplication.class, args);
+    }
+
+}
